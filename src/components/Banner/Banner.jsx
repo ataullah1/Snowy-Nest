f56adb7dbc644e0f4a36ba030566f33d2972689e
Md Ataullah <ataullahm100@gmail.com>
2024-04-08 15:44:06 +0600

    Sowiper addign

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,24 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/bundle';
 const Banner = () => {
   return (
     <div>
-      <h1 className="text-5xl text-center font-bold py-5">
-        This is Banner Section
-      </h1>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        ...
+      </Swiper>
     </div>
   );
 };
