d4689fdfb6e8caeb8712383416c2a4b803d9f06f
Md Ataullah <ataullahm100@gmail.com>
2024-04-09 00:29:56 +0600

    REgister Validation added

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,5 +1,4 @@
 import Nav from '../../components/Nav/Nav';
-import { useContext, useEffect, useState } from 'react';
 import {
   FaEye,
   FaEyeSlash,
@@ -13,34 +12,16 @@ import { RiLockPasswordFill } from 'react-icons/ri';
 import { TbPasswordFingerprint } from 'react-icons/tb';
 import { Link } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
-import image1 from '../../assets/sliderImg/8.jpg';
+import image1 from '../../assets/sliderImg/11.jpg';
+import { useState } from 'react';
 
 const Register = () => {
   const [eye, setEye] = useState(false);
   const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const isValidPass = /^(?=.*[A-Z])(?=.*\d).{6,}$/;
+  const isValidPass = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
   const [emailErr, setEmailErr] = useState(null);
   const [passErr, setPassErr] = useState(null);
   const [confPassErr, setConfPassErr] = useState(null);
-  const [loginErr, setLoginErr] = useState(false);
-
-  const {
-    logout,
-    loding,
-    emailPass,
-    // namePhotoURL,
-    twitterLogin,
-    fbLogin,
-    googleLogin,
-    userDta,
-  } = useContext(ContextAuth);
-  // Naviget, login done then go to Home
-  const naviget = useNavigate();
-  useEffect(() => {
-    if (userDta) {
-      naviget('/');
-    }
-  }, [userDta, naviget]);
 
   const [imgNam, setImgNam] = useState({});
   const handleSignUpSubmit = (e) => {
@@ -55,7 +36,7 @@ const Register = () => {
     const email = formDta.get('email');
     const pass = formDta.get('password');
     const confPass = formDta.get('confirmPass');
-    // console.log(name, photo, email, pass, confPass);
+    console.log(name, photo, imgNam, email, pass, confPass);
 
     if (!isValidEmail.test(email)) {
       setEmailErr('Please enter a valid email address.');
@@ -77,147 +58,157 @@ const Register = () => {
 
   return (
     <div>
-      <div className="bg-slate-600">
-        <Nav />
-      </div>
-      <div className="flex items-center justify-center min-h-[calc(100vh-92px)]">
-        <div className="flex flex-col md:flex-row-reverse items-center w-full gap-5 border-2 rounded-lg border-secondary min-h-[450px] md:p-10 mt-8">
-          <div className="w-full md:w-1/2 h-[300px] md:h-[450px] flex justify-center items-center">
-            <img src={image1} alt="" className="max-h-full mx-auto" />
-          </div>
-          <div className="w-full md:w-1/2 mx-auto border-2 rounded-lg p-5">
-            <h1 className="text-3xl font-bold pb-6">Create an account</h1>
-            <form className="space-y-4" onSubmit={handleSignUpSubmit}>
-              <label className="input input-bordered flex items-center gap-2">
-                <FaUserAlt />
-                <input
-                  type="text"
-                  name="name"
-                  className="grow"
-                  placeholder="Your Name"
-                />
-              </label>
-              <label className="input input-bordered flex items-center gap-2">
-                <FaImage />
-                <input
-                  type="text"
-                  className="grow"
-                  name="img"
-                  placeholder="Your Image URL"
-                />
-              </label>
-              <div>
-                <label
-                  className={
-                    emailErr
-                      ? 'input input-bordered flex items-center gap-2 border-red-500'
-                      : 'input input-bordered flex items-center gap-2'
-                  }
-                >
-                  <MdEmail />
-                  <input
-                    type="email"
-                    name="email"
-                    className="grow"
-                    placeholder="Email"
-                  />
-                </label>
-                {emailErr && (
-                  <p className="text-sm text-red-500 italic">{emailErr}</p>
-                )}
-              </div>
-              <div>
-                <label
-                  className={
-                    passErr
-                      ? 'relative input input-bordered flex items-center gap-2 border-red-500'
-                      : 'relative input input-bordered flex items-center gap-2'
-                  }
-                >
-                  <RiLockPasswordFill />
-                  <input
-                    type={eye ? 'text' : 'password'}
-                    name="password"
-                    className="grow"
-                    required
-                    placeholder="Password"
-                  />
-                  <div
-                    onClick={() => setEye(!eye)}
-                    className="cursor-pointer text-xl absolute right-3"
-                  >
-                    {eye ? <FaEyeSlash /> : <FaEye />}
-                  </div>
-                </label>
-                {passErr && (
-                  <p className="text-sm text-red-500 italic">{passErr}</p>
-                )}
-              </div>
-              <div>
-                <label
-                  className={
-                    confPassErr
-                      ? 'input input-bordered flex items-center gap-2 border-red-500'
-                      : 'input input-bordered flex items-center gap-2'
-                  }
-                >
-                  <TbPasswordFingerprint />
-                  <input
-                    type="password"
-                    className="grow"
-                    name="confirmPass"
-                    placeholder="Confirm Password"
-                  />
-                </label>
-                {confPassErr && (
-                  <p className="text-sm text-red-500 italic">{confPassErr}</p>
-                )}
-              </div>
-
-              <input
-                // onClick={handeleNameImg}
-                type="submit"
-                value="Register"
-                className="w-full py-2 px-4 rounded-md text-center text-white font-bold bg-secondary active:scale-95 duration-150 cursor-pointer hover:bg-[#bb019c]"
-              />
-            </form>{' '}
-            <p className="pt-2">
-              Already have an account?{' '}
-              <Link to={'/login'} className="underline">
-                Login
-              </Link>
-            </p>
-            <div className="divider divider-secondary">Or</div>
-            <div className="flex flex-col gap-2">
-              <button
-                onClick={() => socialLogin('handleGoogleLogin')}
-                className="py-2 px-4 w-full font-medium border-y hover:shadow-lg shadow-indigo-900/20 rounded-md flex items-center justify-center gap-2 bg-white"
-              >
-                <span>
-                  <FcGoogle />
-                </span>
-                Login With Google
-              </button>
-              <button
-                onClick={() => socialLogin('handleGithuLogin')}
-                className="py-2 px-4 w-full font-medium border-y hover:shadow-lg shadow-blue-500/20 rounded-md  flex items-center justify-center gap-2"
-              >
-                <span className="text-blue-500">
-                  <FaFacebook />
-                </span>
-                Login With GitHub
-              </button>
-              <button
-                onClick={() => socialLogin('handleTwitterLogin')}
-                className="py-2 px-4 w-full font-medium border-y hover:shadow-lg shadow-blue-400-900/20 rounded-md  flex items-center justify-center gap-2"
-              >
-                <span className="text-blue-400">
-                  <FaTwitter />
-                </span>
-                Login With Twitter
-              </button>
+      <div>
+        <div
+          className="h-72 overflow-hidden bg-cover bg-no-repeat p-12 text-center"
+          style={{
+            backgroundImage: `url(${image1})`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          <div
+            className="absolute bottom-0 left-0 right-0 top-0 h-72 w-full overflow-hidden bg-fixed"
+            style={{ backgroundColor: ' rgba(0, 0, 0, 0.6)' }}
+          >
+            <div className="bg-transparent w-full z-10">
+              <Nav />
             </div>
           </div>
+        </div>
+      </div>
+      <div className="w-full mt-2 md:w-1/2 mx-auto border-2 rounded-lg p-5">
+        <h1 className="text-3xl font-bold pb-6">Create an account</h1>
+        <form className="space-y-4" onSubmit={handleSignUpSubmit}>
+          <label className="input input-bordered flex items-center gap-2">
+            <FaUserAlt />
+            <input
+              type="text"
+              name="name"
+              className="grow"
+              placeholder="Your Name"
+            />
+          </label>
+          <label className="input input-bordered flex items-center gap-2">
+            <FaImage />
+            <input
+              type="text"
+              className="grow"
+              name="img"
+              placeholder="Your Image URL"
+            />
+          </label>
+          <div>
+            <label
+              className={
+                emailErr
+                  ? 'input input-bordered flex items-center gap-2 border-red-500'
+                  : 'input input-bordered flex items-center gap-2'
+              }
+            >
+              <MdEmail />
+              <input
+                type="email"
+                name="email"
+                className="grow"
+                placeholder="Email"
+              />
+            </label>
+            {emailErr && (
+              <p className="text-sm text-red-500 italic">{emailErr}</p>
+            )}
+          </div>
+          <div>
+            <label
+              className={
+                passErr
+                  ? 'relative input input-bordered flex items-center gap-2 border-red-500'
+                  : 'relative input input-bordered flex items-center gap-2'
+              }
+            >
+              <RiLockPasswordFill />
+              <input
+                type={eye ? 'text' : 'password'}
+                name="password"
+                className="grow"
+                required
+                placeholder="Password"
+              />
+              <div
+                onClick={() => setEye(!eye)}
+                className="cursor-pointer text-xl absolute right-3"
+              >
+                {eye ? <FaEye /> : <FaEyeSlash />}
+              </div>
+            </label>
+            {passErr && (
+              <p className="text-sm text-red-500 italic">{passErr}</p>
+            )}
+          </div>
+          <div>
+            <label
+              className={
+                confPassErr
+                  ? 'input input-bordered flex items-center gap-2 border-red-500'
+                  : 'input input-bordered flex items-center gap-2'
+              }
+            >
+              <TbPasswordFingerprint />
+              <input
+                type="password"
+                className="grow"
+                name="confirmPass"
+                placeholder="Confirm Password"
+              />
+            </label>
+            {confPassErr && (
+              <p className="text-sm text-red-500 italic">{confPassErr}</p>
+            )}
+          </div>
+
+          <input
+            // onClick={handeleNameImg}
+            type="submit"
+            value="Register"
+            className="w-full py-2 px-4 rounded-md text-center text-white hover:text-redLi font-bold bg-redLi active:scale-95 duration-150 cursor-pointer hover:bg-transparent border-2 border-redLi"
+          />
+        </form>{' '}
+        <p className="pt-2">
+          Already have an account?{' '}
+          <Link to={'/login'} className="underline text-redLi">
+            Login
+          </Link>
+        </p>
+        <div className="divider divider-secondary">Or</div>
+        <div className="flex flex-col gap-2">
+          <button
+            onClick={() => socialLogin('handleGoogleLogin')}
+            className="py-2 px-4 w-full font-medium border-y hover:shadow-lg shadow-indigo-900/20 rounded-md flex items-center justify-center gap-2 bg-white"
+          >
+            <span>
+              <FcGoogle />
+            </span>
+            Login With Google
+          </button>
+          <button
+            onClick={() => socialLogin('handleGithuLogin')}
+            className="py-2 px-4 w-full font-medium border-y hover:shadow-lg shadow-blue-500/20 rounded-md  flex items-center justify-center gap-2"
+          >
+            <span className="text-blue-500">
+              <FaFacebook />
+            </span>
+            Login With GitHub
+          </button>
+          <button
+            onClick={() => socialLogin('handleTwitterLogin')}
+            className="py-2 px-4 w-full font-medium border-y hover:shadow-lg shadow-blue-400-900/20 rounded-md  flex items-center justify-center gap-2"
+          >
+            <span className="text-blue-400">
+              <FaTwitter />
+            </span>
+            Login With Twitter
+          </button>
         </div>
       </div>
     </div>
