28f564134597b975892748821ce73909976714ba
Md Ataullah <ataullahm100@gmail.com>
2024-04-08 16:12:47 +0600

    Readme.md Package name added and Swiper slied added:

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 <pre><code>npm install -D tailwindcss postcss autoprefixer</code></pre>
+
+## Package Used-
+
+- React Router Dom
+- react-icons
+- react-hot-toast
+- swiper
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,23 +3,37 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/bundle';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Autoplay, Pagination, Navigation, Keyboard } from 'swiper/modules';
+
 const Banner = () => {
   return (
-    <div>
+    <>
       <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        keyboard={{
+          enabled: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation, Keyboard]}
+        className="mySwiper min-h-screen"
       >
         <SwiperSlide>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        ...
       </Swiper>
-    </div>
+    </>
   );
 };
 
