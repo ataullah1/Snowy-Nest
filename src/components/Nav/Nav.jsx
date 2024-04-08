2b6e71800a256f5e1a87f15c67b330f24aa831f0
Md Ataullah <ataullahm100@gmail.com>
2024-04-08 14:26:24 +0600

    Navber designing

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,137 +1,144 @@
 import { Toaster } from 'react-hot-toast';
 import { Link, NavLink } from 'react-router-dom';
-import ProfileMenu from '../ProfileMenu/ProfileMenu';
-import { useState } from 'react';
+import logo from '../../assets/whiteLogo.png';
+// import ProfileMenu from '../ProfileMenu/ProfileMenu';
+// import { useState } from 'react';
 // import { ContextProvider } from '../../provider/AuthProvider';
-import { IoMdClose } from 'react-icons/io';
+// import { IoMdClose } from 'react-icons/io';
 
 const Nav = () => {
   // const { user } = useContext(ContextProvider);
-  const [view, setView] = useState(false);
+  // const [view, setView] = useState(false);
   return (
-    <div>
-      <Toaster />
-      <div className="navbar flex items-center flex-col sm:flex-row sm:justify-between bg-base-100 ">
-        <div className="navbar-start flex items-center flex-row-reverse justify-between w-full sm:w-auto sm:flex-row">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-7 w-7 rotate-180 sm:rotate-0"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+    <div className="bg-slate-400">
+      <div className="w-11/12 mx-auto ">
+        <Toaster />
+        <div className=" navbar flex items-center flex-col sm:flex-row sm:justify-between">
+          <div className="navbar-start flex items-center flex-row-reverse justify-between w-full sm:w-auto sm:flex-row">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7 rotate-180 sm:rotate-0"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 ml-[-140px] sm:ml-0 z-[1] p-2 shadow rounded-box w-52 border-2 border-solid border-redLi bg-slate-400"
+              >
+                <NavLink
+                  to={'/'}
+                  className="hover:bg-slate-300 py-2 px-4 rounded"
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  to={'about'}
+                  className="hover:bg-slate-300 py-2 px-4 rounded"
+                >
+                  About
+                </NavLink>
+                <NavLink
+                  to={'contact'}
+                  className="hover:bg-slate-300 py-2 px-4 rounded"
+                >
+                  Contact
+                </NavLink>
+                <div className="flex flex-col-reverse gap-2 mt-2">
+                  <Link
+                    to={'login'}
+                    className="font-semibold hover:bg-black hover:text-white px-5 sm:px-8 py-2 sm:py-3 rounded-md border border-black active:scale-90 duration-150"
+                  >
+                    Login
+                  </Link>
+                  <Link
+                    to={'register'}
+                    className="font-semibold bg-black text-white px-5 sm:px-8 py-2 sm:py-3 rounded-md active:scale-90 duration-150"
+                  >
+                    Register
+                  </Link>
+                </div>
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 ml-[-140px] sm:ml-0 z-[1] p-2 shadow bg-base-100 rounded-box w-52 border-2 border-solid border-orange-400"
-            >
-              <NavLink
-                to={'/'}
-                className="hover:bg-slate-300 py-2 px-4 rounded"
-              >
+            <Link to={'/'} className="text-3xl font-extrabold">
+              <img className="h-12 lg:h-[60px]" src={logo} alt="" />
+            </Link>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 flex gap-6">
+              <NavLink className="px-6 py-3 font-semibold hover:bg-slate-300 rounded-md">
                 Home
               </NavLink>
               <NavLink
                 to={'about'}
-                className="hover:bg-slate-300 py-2 px-4 rounded"
+                className="px-6 py-3 font-semibold hover:bg-slate-300 rounded-md"
               >
                 About
               </NavLink>
               <NavLink
                 to={'contact'}
-                className="hover:bg-slate-300 py-2 px-4 rounded"
+                className="px-6 py-3 font-semibold hover:bg-slate-300 rounded-md"
               >
                 Contact
               </NavLink>
-              <div className="flex flex-col-reverse gap-2 mt-2">
-                <Link
-                  to={'login'}
-                  className="font-semibold hover:bg-black hover:text-white px-5 sm:px-8 py-2 sm:py-3 rounded-md border border-black active:scale-90 duration-150"
-                >
-                  Login
-                </Link>
-                <Link
-                  to={'register'}
-                  className="font-semibold bg-black text-white px-5 sm:px-8 py-2 sm:py-3 rounded-md active:scale-90 duration-150"
-                >
-                  Register
-                </Link>
-              </div>
             </ul>
           </div>
-          <Link to={'/'} className="text-3xl font-extrabold">
-            SnowyFire
-          </Link>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1 flex gap-6">
-            <NavLink className="px-6 py-3 font-semibold hover:bg-slate-300 rounded-md">
-              Home
-            </NavLink>
-            <NavLink
-              to={'about'}
-              className="px-6 py-3 font-semibold hover:bg-slate-300 rounded-md"
-            >
-              About
-            </NavLink>
-            <NavLink
-              to={'contact'}
-              className="px-6 py-3 font-semibold hover:bg-slate-300 rounded-md"
-            >
-              Contact
-            </NavLink>
-          </ul>
-        </div>
-        {user ? (
-          <div className="navbar-end flex gap-4 w-auto">
-            <img
-              onClick={() => setView(!view)}
-              src={user.photoURL}
-              className="border border-orange-400 rounded-full h-11 w-11 cursor-pointer p-1"
-              alt=""
-            />
+          {/* {user ? ( */}
+          {/* <div className="navbar-end flex gap-4 w-auto">
+          <img
+            onClick={() => setView(!view)}
+            src={user.photoURL}
+            className="border border-redLi rounded-full h-11 w-11 cursor-pointer p-1"
+            alt=""
+          />
 
-            <div
-              className={`absolute top-16 lg:top-20 right-3 duration-3000 transition-transform ${
-                view ? 'translate-x-0 visible' : 'translate-x-96 invisible'
-              }`}
-            >
-              <div className="relative">
-                <div
-                  className="text-white absolute text-2xl top-3 left-3 border rounded cursor-pointer hover:border-orange-400 duration-150"
-                  onClick={() => setView(!view)}
-                >
-                  <IoMdClose />
-                </div>
-                <ProfileMenu />
+          <div
+            className={`absolute top-16 lg:top-20 right-3 duration-3000 transition-transform ${
+              view ? 'translate-x-0 visible' : 'translate-x-96 invisible'
+            }`}
+          >
+            <div className="relative">
+              <div
+                className="text-white absolute text-2xl top-3 left-3 border rounded cursor-pointer hover:border-orange-400 duration-150"
+                onClick={() => setView(!view)}
+              >
+                <IoMdClose />
               </div>
+              <ProfileMenu />
             </div>
           </div>
-        ) : (
+        </div> */}
+          {/* ) : ( */}
           <div className="navbar-end gap-4 hidden sm:flex w-auto">
             <Link
               to={'login'}
-              className="font-semibold hover:bg-black hover:text-white px-5 sm:px-8 py-2 sm:py-3 rounded-md border border-black active:scale-90 duration-150"
+              className="font-semibold hover:bg-white hover:text-redLi px-5 sm:px-8 py-2 sm:py-3 rounded-md border border-redLi active:scale-90 duration-150 text-redLi"
             >
               Login
             </Link>
             <Link
               to={'register'}
-              className="font-semibold bg-black text-white px-5 sm:px-8 py-2 sm:py-3 rounded-md active:scale-90 duration-150"
+              className="font-semibold bg-white text-redLi px-5 sm:px-8 py-2 sm:py-3 rounded-md active:scale-90 duration-150"
             >
               Register
             </Link>
           </div>
-        )}
+          {/* )} */}
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        redLi: '#ff6b6b',
+      },
+    },
   },
   plugins: [require('daisyui')],
 };
