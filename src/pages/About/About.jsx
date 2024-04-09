ab50ad2b412d05ab1bfc5088932e0ee37a108c27
Md Ataullah <ataullahm100@gmail.com>
2024-04-09 14:39:02 +0600

    About page and contact page navber added

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,9 @@
+import Nav from '../../components/Nav/Nav';
+
 const About = () => {
   return (
     <div>
+      <Nav />
       <h1 className="text-6xl font-bold text-center py-3">
         This is About Section.
       </h1>
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,6 +1,9 @@
+import Nav from '../../components/Nav/Nav';
+
 const Contact = () => {
   return (
     <div>
+      <Nav />
       <h1 className="text-5xl text-center font-bold py-5">
         This is Contact Section
       </h1>
