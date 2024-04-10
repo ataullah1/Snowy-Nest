abd872aac21eb64239cab8607175c15909f0162d
Md Ataullah <ataullahm100@gmail.com>
2024-04-11 00:02:48 +0600

    future propertices section adding

--- src/components/Propertices/Propertices.jsx
+++ src/components/Propertices/Propertices.jsx
@@ -1,8 +1,14 @@
 const Propertices = () => {
   return (
     <div>
-      <h1 className="pt-16 text-3xl text-center font-bold">Out Featured Properties</h1>
-
+      <h1 className="pt-16 text-3xl text-center font-bold">
+        Out Featured Properties
+      </h1>
+      <p className="text-base text-center max-w-[550px] mx-auto">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum eligendi
+        delur distinctio iusto? Tempora numquam non quos!
+      </p>
+      <div>bal</div>
     </div>
   );
 };
