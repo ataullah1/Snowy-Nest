e7187bcf6a33ce0f2366d468b9b17e09ea54d6c1
Md Ataullah <ataullahm100@gmail.com>
2024-04-12 08:42:34 +0600

    Propertices Card added

--- src/components/Propertices/PropertiCard.jsx
+++ src/components/Propertices/PropertiCard.jsx
@@ -1,10 +1,13 @@
 import { FaRegHeart } from 'react-icons/fa';
 import card1 from '../../assets/sliderImg/card/1.jpg';
+import { SlSizeFullscreen } from 'react-icons/sl';
+import { LuBedSingle } from 'react-icons/lu';
+import { FaBath } from 'react-icons/fa6';
 // import card2 from '../../assets/sliderImg/card/2.jpg';
 
 const PropertiCard = () => {
   return (
-    <div className="bg-white max-w-[500px] mx-auto p-3 border rounded-lg hover:scale-105 duration-200">
+    <div className="bg-white max-w-[500px] mx-auto p-3 border rounded-lg hover:scale-105 duration-200 space-y-3">
       <div className="relative">
         <div className="relative">
           <img className="rounded-lg " src={card1} alt="" />
@@ -14,7 +17,7 @@ const PropertiCard = () => {
           <h1 className="text-2xl text-white font-bold">
             $13,000<span className="text-lg">/mo</span>
           </h1>
-          <span className="text-white bg-[#17161689] p-2 rounded-md cursor-pointer">
+          <span className="text-white hover:text-redLi bg-[#17161689] p-2 rounded-md cursor-pointer">
             <FaRegHeart />
           </span>
         </div>
@@ -22,23 +25,42 @@ const PropertiCard = () => {
           For Rent
         </button>
       </div>
-      <h1 className="text-2xl font-bold text-slate-600">
-        Lorem ipsum dolor sit amet.
-      </h1>
+      <div className="space-y-2 px-3">
+        <p className="text-redLi font-semibold">Residential</p>
+        <h1 className="text-2xl font-bold text-black">
+          Lorem ipsum dolor sit amet.
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis,
+          quisquam?
+        </p>
+        <hr />
+        <div className="flex items-center justify-between py-2 gap-3 text-redLi">
+          <div className="bg-[#3bf5f853] w-full py-1 px-2 flex items-center justify-center rounded gap-2 text-base font-semibold">
+            <span className="text-sm">
+              <SlSizeFullscreen />
+            </span>
+            600 Sq Ft
+          </div>
+          <div className="bg-[#c9f15b67] w-full py-1 px-2 flex items-center justify-center rounded gap-2 text-base font-semibold">
+            <span>
+              <LuBedSingle />
+            </span>
+            4 Beds
+          </div>
+          <div className="bg-[#25eb8867] w-full py-1 px-2 flex items-center justify-center rounded gap-2 text-base font-semibold">
+            <span>
+              <FaBath />
+            </span>
+            3 Baths
+          </div>
+        </div>
+        <hr className="pb-3" />
+        <button className="w-full py-2 px-3 rounded-md text-base font-bold border-redLi border-2 text-redLi active:scale-95 duration-150">
+          View Property
+        </button>
+      </div>
     </div>
-    // <div>
-    //   <div
-    //     className="relative h-72 rounded-lg overflow-hidden bg-cover bg-no-repeat p-12 text-center"
-    //     style={{
-    //       backgroundImage: `url(${card2})`,
-    //       backgroundPosition: 'center',
-    //       backgroundSize: 'cover',
-    //       backgroundRepeat: 'no-repeat',
-    //     }}
-    //   >
-    //     <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full bg-[#0000004e] z-30"></div>
-    //   </div>
-    // </div>
   );
 };
 
