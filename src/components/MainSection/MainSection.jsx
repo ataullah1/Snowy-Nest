2a6a13d856f02ad2abf314d7f43c94dd30252470
Md Ataullah <ataullahm100@gmail.com>
2024-04-14 15:00:42 +0600

    Our Value section adding

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,9 +1,11 @@
+import OurValue from '../OurValue/OurValue';
 import Propertices from '../Propertices/Propertices';
 
 const MainSection = () => {
   return (
     <div className="bg-[#F7F7F7] pb-16">
       <Propertices />
+      <OurValue />
     </div>
   );
 };
---
+++ src/components/OurValue/OurValue.jsx
@@ -0,0 +1,9 @@
+const OurValue = () => {
+  return (
+    <div>
+      <h1>Our value</h1>
+    </div>
+  );
+};
+
+export default OurValue;
