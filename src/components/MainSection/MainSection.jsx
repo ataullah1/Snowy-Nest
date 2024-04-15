aeba5f093ee3a6fe18fa64505431f4c5a7f1c8ce
Md Ataullah <ataullahm100@gmail.com>
2024-04-15 13:15:42 +0600

    office location added'

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,13 +1,15 @@
 import Contact from '../ContactUs/Contact';
+import OfficeMap from '../OfficeMap/OfficeMap';
 import OurValue from '../OurValue/OurValue';
 import Propertices from '../Propertices/Propertices';
 
 const MainSection = () => {
   return (
-    <div className="bg-[#F7F7F7] pb-16 ">
+    <div className="bg-[#F7F7F7]">
       <Propertices />
       <OurValue />
-      <Contact/>
+      <Contact />
+      <OfficeMap />
     </div>
   );
 };
