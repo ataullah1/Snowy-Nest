f741601eb272b743ef4a7ae6b37acd9c279538ed
Md Ataullah <ataullahm100@gmail.com>
2024-04-15 13:27:10 +0600

    COntact Page added

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,14 @@
+import ContactSec from '../../components/ContactSec/ContactSec';
+import OfficeMap from '../../components/OfficeMap/OfficeMap';
+
 const Contact = () => {
   return (
     <div>
       <div className="bg-slate-900 h-20"></div>
-      <div></div>
+      <div>
+        <ContactSec />
+        <OfficeMap />
+      </div>
     </div>
   );
 };
